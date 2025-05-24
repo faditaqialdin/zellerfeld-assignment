2b55306fb57f764d1036b6188517e90d6aa8b7b0
Mohammad Fadi Taqi Al-Din <m.fadi313@gmail.com>
2025-05-24 17:08:13 +0300

    add api routes versioning and names

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [UserController::class, 'register']);
+Route::prefix('v1')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [UserController::class, 'register'])->name('register');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    });
 });
